4bf55875430d8f573bee6a7440a861ab6f7dbc14
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-13 13:13:16 +0300

    KangarooController created with PostConstructor

---
+++ src/main/java/com/workintech/zoo/controller/KangarooController.java
@@ -0,0 +1,23 @@
+package com.workintech.zoo.controller;
+
+import com.workintech.zoo.entity.Kangaroo;
+import com.workintech.zoo.enums.Sex;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/zoo")
+public class KangarooController {
+    private Map<Long, Kangaroo> kangaroos;
+
+    @PostConstruct
+    public void kangarooInitialization() {
+        kangaroos = new HashMap<>();
+        Kangaroo kangaroo = new Kangaroo(1L, "Sammy", 2.4, 40.8, Sex.FEMALE, false);
+        kangaroos.put(kangaroo.getId(), kangaroo);
+    }
+}
