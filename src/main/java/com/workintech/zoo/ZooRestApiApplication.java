1ac178b7f6cd4b8d168fb213c18fad62be7a1d5f
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-13 11:55:29 +0300

    Cloned

---
+++ src/main/java/com/workintech/zoo/ZooRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooRestApiApplication.class, args);
+	}
+
+}
